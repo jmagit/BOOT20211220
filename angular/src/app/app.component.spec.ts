16fc0230c6597cc334033ed5c58d404d75d4cc4d
Javier <jmartin@grupoloyal.com>
2022-01-12 18:15:39 +0100

    Inicio

--- angular/src/app/app.component.spec.ts
+++ angular/src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'curso'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('curso');
+    expect(app.title).toEqual('cursos');
   });
 
   it('should render title', () => {
