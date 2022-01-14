494fd25c3cbbf08cf1efc5289d50c15045868ea9
Javier <jmartin@grupoloyal.com>
2022-01-14 12:33:13 +0100

    estilo

--- angular/src/lib/my-core/index.ts
+++ angular/src/lib/my-core/index.ts
@@ -1,2 +1,3 @@
 export { MyCoreModule } from './modules/my-core.module'
 export * from './services/logger.service'
+export { ElipsisPipe, CapitalizePipe } from './pipes/cadenas.pipe'
--- angular/src/lib/my-core/modules/my-core.module.ts
+++ angular/src/lib/my-core/modules/my-core.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PIPES_CADENAS } from '../pipes/cadenas.pipe';
+import { SizerComponent } from '../components/sizer.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PIPES_CADENAS, SizerComponent,
+  ],
+  exports: [
+    PIPES_CADENAS, SizerComponent,
+  ],
   imports: [
     CommonModule
   ]
